48bdca6284ac9c0e9ada95ad5a71bc36d3d725bd
Compotation <46853874+Compotation@users.noreply.github.com>
2023-05-16 19:01:23 -0400

    main

--- main.cpp
+++ main.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
+#include "Node.h"
+#include "List.h"
+#include "Dictionary.h"
 
 int main() {
+  auto p1 = new Person("Bryant", "Zheng", 0);
+  auto p3 = new Person("Mike", "Zamansky", 1);
+  auto p2 = new Person(*p1);
+  auto d1 = new Dictionary();
+  std::cout << d1->allKeys() << "\n";
+  std::cout << d1->sfold("cat") << "\n";
+  std::cout << d1->sfold("mad") << "\n";
+  std::cout << d1->insert(p1) << "\n";
+  std::cout << d1->insert(p3) << "\n";
+//  std::cout << d1->insert(p2) << "\n";
+  std::cout << d1->get("Zheng, Bryant")->get_id() << "\n";
+  std::cout << d1->allKeys() << "\n";
+
   return 0;
 }
