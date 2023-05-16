f7a2b7dfd8e3f0309d565cf9b55ecaa23508f667
Compotation <46853874+Compotation@users.noreply.github.com>
2023-05-16 18:28:59 -0400

    retrieve test

--- tests.cpp
+++ tests.cpp
@@ -12,3 +12,18 @@ TEST_CASE("insert") {
   d1->insert(p2);
   CHECK_EQ(d1->get(p2->get_name()), p2);
 }
+
+TEST_CASE("retrieve") {
+  auto d1 = new Dictionary();
+  CHECK_EQ(d1->get(", Ko"), nullptr);
+  auto p3 = new Person("Ko", "", 2);
+  d1->insert(p3);
+  CHECK_EQ(d1->get(", Ko"), p3);
+  auto p1 = new Person("Bryant", "Zheng", 0);
+  auto p2 = new Person("Mike", "Zamansky", 1);
+  d1->insert(p1);
+  CHECK_EQ(d1->get(p1->get_name()), p1);
+  d1->insert(p2);
+  CHECK_EQ(d1->get(p2->get_name()), p2);
+
+}
