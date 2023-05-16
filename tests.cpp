becb243e371ca1478ca5f502cc2d2ac9a891178a
Compotation <46853874+Compotation@users.noreply.github.com>
2023-05-16 18:57:59 -0400

    test allKeys

--- tests.cpp
+++ tests.cpp
@@ -25,5 +25,17 @@ TEST_CASE("retrieve") {
   CHECK_EQ(d1->get(p1->get_name()), p1);
   d1->insert(p2);
   CHECK_EQ(d1->get(p2->get_name()), p2);
+}
 
+TEST_CASE("get all keys") {
+  auto d1 = new Dictionary();
+  CHECK_EQ(d1->allKeys(), "");
+  auto p1 = new Person("Bryant", "Zheng", 0);
+  auto p2 = new Person("Mike", "Zamansky", 1);
+  auto p3 = new Person("Ko", "", 2);
+  d1->insert(p1);
+  d1->insert(p2);
+  d1->insert(p3);
+  std::cout << d1->allKeys() << "\n";
+  CHECK_EQ(d1->allKeys(), "Zamansky, Mike;, Ko;Zheng, Bryant;");
 }
